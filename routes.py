83ff4268af6a75f98e345cf71bd02e6f3a866b64
John Kwon <jykwon728@gmail.com>
2018-06-16 16:06:20 +0100

    routes typo fixed

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/leartningflask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
 
 @app.route("/")
 def index():
